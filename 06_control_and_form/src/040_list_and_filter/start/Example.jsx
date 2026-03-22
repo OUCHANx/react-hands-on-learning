e84786596a08ad8dff07a5ca25074f47d0193ae1
ouchan <kingbook0628@gmail.com>
2026-03-22 09:30:25 +0900

    filterメソッド

--- 06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ 06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -3,13 +3,24 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterval, setFilterval] = useState("");
+  console.log(animals.filter(animal => animal === "Dog"));
   return (
     <>
       <h3>配列のフィルター</h3>
+  <input type="text" value ={filterval} onChange={(e)=>
+    setFilterval(e.target.value)} />
+
       <ul>
         {animals
+          .filter(animal => {
+            const isMatch = animal.indexOf(filterval) !==
+            -1;
+            console.log(animal.indexOf(filterval));
+            return isMatch;
+          })
           .map((animal) => (
-          <li>{animal}</li>
+          <li key = {animal}>{animal}</li>
         ))}
       </ul>
     </>
@@ -17,3 +28,7 @@ const Example = () => {
 };
 
 export default Example;
+//indexOfは、文字列の中に特定の文字列が存在するかどうかを
+// 調べるためのメソッドです。存在する場合は、その文字列の最初の
+// 位置を返し、存在しない場合は-1を返します。上記のコードでは、
+// filtervalがanimalの中に含まれているかどうかを確認しています。
