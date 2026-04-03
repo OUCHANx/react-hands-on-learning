f8d364692574111415a0ced4cc281c52f468d71c
ouchan <kingbook0628@gmail.com>
2026-04-03 14:45:54 +0900

    useReducerについて学んだ

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,9 +1,40 @@
+import { useReducer } from "react";
+import { useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, { type, step }) => {
+    switch(type){
+    case "+":
+      return prev + step;
+    case"-":
+    return prev - step;
+    default:
+      throw new Error('不明なアクション')
+    }
+  },0);
+  const countUp = () => {
+    setState(prev => ++prev)
+    //setState({
+    //return prev = prev + 1;
+  //})
+  };
+  const rcountUp = () => {
+    dispatch({type : "+", step: 2 });
+  };
+  const rcountDown = () => {
+    dispatch({type : "-", step: 3 });
+  };
   return (
     <>
+    <h3>{state}</h3>
+    <button onClick={countUp}>+</button>
+    <h3>{rstate}</h3>
+    <button onClick={rcountUp}>+</button>
+    <button onClick={rcountDown}>-</button>
     </>
   );
 };
 
 export default Example;
+//どこが間違ってる？＝＞ 
